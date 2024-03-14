6945d01b17d1071981aa0af9a769e41c3b2adbd8
PeachBlack-Alba <peachblackweb@gmail.com>
2024-03-14 15:49:03 +0100

    added apptheme

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'core/utils/theme/app_theme.dart';
 import 'features/search/domain/repository/data_repository.dart';
 import 'features/home/home_page.dart';
 import 'features/search/presentation/bloc/repo_search_bloc.dart';
@@ -19,9 +20,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'GitHub Search',
-      theme: ThemeData(
-        primarySwatch: Colors.grey,
-      ),
+      theme: AppTheme.darkTheme,
       home: BlocProvider(
         create: (context) => RepoSearchBloc(DataRepository()),
         child: Home(),
